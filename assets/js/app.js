f7a14e1bd443fae9ef625c3ac9b4ab9896e0b4e9
Shahaliyev <shahaliyev99@gmail.com>
2025-02-16 02:48:22 +0400

    minor fixes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,15 +1,23 @@
+
+
 let scrollBtn = document.getElementById("scroll-btn");
+let lastScrollTop = 0;
 
 window.onscroll = function() {
-  scrollFunction();
-};
+  let currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
 
-function scrollFunction() {
-  if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
-    scrollBtn.style.display = "block";
-  } else {
+  if (currentScroll <= 1000) {
+   
     scrollBtn.style.display = "none";
+  } else if (currentScroll > lastScrollTop) {
+   
+    scrollBtn.style.display = "none";
+  } else {
+   
+    scrollBtn.style.display = "block";
   }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
 }
 
 function scrollToTop() {
